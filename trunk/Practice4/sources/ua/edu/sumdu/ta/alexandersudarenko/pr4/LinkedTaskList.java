c95952d9e0c8d88e61d9e54293a55e75586e0df1
sudar69@gmail.com <sudar69@gmail.com@55a6e225-9056-6fd4-6e9b-9218a8b82b6f>
2013-10-28 17:26:54 +0000

    inner class element

--- trunk/Practice4/sources/ua/edu/sumdu/ta/alexandersudarenko/pr4/LinkedTaskList.java
+++ trunk/Practice4/sources/ua/edu/sumdu/ta/alexandersudarenko/pr4/LinkedTaskList.java
@@ -91,4 +91,38 @@ public class LinkedTaskList extends AbstractTaskList {
         return tempArrayList;
     }
 
+    
+    public class Element {
+        private Element next = null;
+        private Task element = null;
+
+        /**
+         * @param task Set Task
+         */
+        public void setElement(Task task) {
+            this.element = task;
+        } 
+        
+        /**
+         * @param next Set Element
+         */
+        public void setNext(Element next) {
+            this.next = next;
+        }         
+
+        /**
+         * @return Returns Task
+         */
+        public Task getElement() {
+            return this.element;
+        } 
+            
+        /**
+         * @return Return next Element
+         */
+        public Element getNext() {
+            return this.next;
+        }
+    }    
+    
 }

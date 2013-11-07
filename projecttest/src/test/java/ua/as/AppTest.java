f5a579520d4a7f57c9ac5d5eaa58ea47548999d8
sudar69 <sudar69@gmail.com>
2013-11-07 10:51:18 +0000

    Add manev test

---
+++ projecttest/src/test/java/ua/as/AppTest.java
@@ -0,0 +1,83 @@
+package ua.as;
+
+import java.io.File;
+import java.io.IOException;
+ 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class AppTest {
+	 private static ChromeDriverService service;
+     public static WebDriver driver;
+	 private static String PATH_TO_CHROMEDRIVER_EXE = "chromedriver.exe";
+ 
+     @BeforeClass
+     public static void createAndStartService() throws IOException {
+         service = new ChromeDriverService.Builder()
+            .usingDriverExecutable(new File(PATH_TO_CHROMEDRIVER_EXE))
+            .usingAnyFreePort()
+            .build();
+         service.start();
+     }
+ 
+     @Before
+     public void setUp(){
+         driver = new ChromeDriver(service);
+     }
+ 
+     @Test
+     public void simpleTest() {     
+
+		// And now use this to visit Google
+        driver.get("http://www.google.com");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+		
+		// Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+		 
+     }
+ 
+     @After
+     public void tearDown(){
+         driver.quit();
+     }
+ 
+     @AfterClass
+     public static void createAndStopService() {
+         service.stop();
+     }
+}
